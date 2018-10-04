44d3c208434789e7331c608d41c1c57b5e78f69c
Rome314 <401206176@mail.ru>
2018-10-04 21:20:54 +0300

    Task 4 is fixed

--- week7/ex5.c
+++ week7/ex5.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
+#include <malloc.h>
 #include <stdlib.h>
+#include <string.h>
+void *my_realloc(void *ptr, int new_size){
+    
+    char *new_ptr;
+    
+    if(ptr == NULL){
+        return malloc(new_size);
+    }
+    else if(new_size == 0){
+        free(ptr);
+        return NULL;
+    }
+    
+    new_ptr = malloc(new_size);
+    memcpy(new_ptr, ptr, new_size);
+    free(ptr);
+    return new_ptr;
+}
 
 
-int main() {
-
-    char **s = malloc(0);
-    char foo[] = "Hello World";
-
-    *s = foo;
-    printf("s is %s\n", *s);
-
-    s[0] = foo;
-    printf("s[0] is %s\n", s[0]);
-    return (0);
-
+int main(){
+    
+    char *tmp = malloc(1*sizeof(char *));
+    printf("Old size is: %d\n",malloc_usable_size(tmp));
+    tmp[0] = ',';
+    tmp[1] = '\0';
+    
+    printf("Initial data: %s\n", tmp);
+    
+    tmp = my_realloc(tmp, 10000*sizeof(char));
+    tmp[8] = "\0";
+    printf("New size is: %d\n",malloc_usable_size(tmp));
+    printf("New data: %s\n", tmp);
+    
+	return 0;
 }
