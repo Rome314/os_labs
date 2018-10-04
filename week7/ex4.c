baa0c1a8d21f0e4aa003532f196fe0dba76d9933
Rome314 <401206176@mail.ru>
2018-10-04 21:27:03 +0300

    Task 4 is fixed

--- week7/ex4.c
+++ week7/ex4.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <malloc/malloc.h>
+#include <malloc.h>
 #include <stdlib.h>
 #include <string.h>
 void *my_realloc(void *ptr, int new_size){
     
     char *new_ptr;
     
-    if(*new_ptr == NULL){
+    if(ptr == NULL){
         return malloc(new_size);
     }
     else if(new_size == 0){
@@ -23,15 +23,16 @@ void *my_realloc(void *ptr, int new_size){
 
 int main(){
     
-    char *tmp = calloc(2, sizeof(char *));
-    printf("Old size is: %d\n",malloc_size(tmp));
+    char *tmp = malloc(1*sizeof(char *));
+    printf("Old size is: %d\n",malloc_usable_size(tmp));
     tmp[0] = ',';
     tmp[1] = '\0';
     
     printf("Initial data: %s\n", tmp);
     
-    tmp = my_realloc(tmp, 10*sizeof(char));
-    printf("New size is: %d\n",malloc_size(tmp));
+    tmp = my_realloc(tmp, 10000*sizeof(char));
+    tmp[8] = "\0";
+    printf("New size is: %d\n",malloc_usable_size(tmp));
     printf("New data: %s\n", tmp);
     
 	return 0;
